a17ad738a6add797c70def820f489e18d4e2b439
Marif <marif226@mail.ru>
2022-04-28 19:51:43 +0600

    Config

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
 	"time"
+
 	"github.com/julienschmidt/httprouter"
+	"github.com/marif226/stolik/internal/config"
 	"github.com/marif226/stolik/internal/user"
 	"github.com/marif226/stolik/pkg/logging"
 )
@@ -15,29 +22,55 @@ func main() {
 
 	router := httprouter.New()
 
+	cfg := config.GetConfig()
+
 	logger.Info("register user handler")
 	handler := user.NewHandler(logger)
 	handler.Register(router)
 
-	start(router)
+	start(router, cfg)
 }
 
 // Start starts the application
-func start(router *httprouter.Router) {
+func start(router *httprouter.Router, cfg *config.Config) {
 	logger := logging.GetLogger()
-
 	logger.Info("start application")
-	listener, err := net.Listen("tcp", ":1234")
-	if err != nil {
-		panic(err)
+
+	var listener net.Listener
+	var listenErr error
+
+	if cfg.Listen.Type == "sock" {
+		logger.Info("detect app path")
+
+		appDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			logger.Fatal(err)
+		}
+		logger.Info("create socket")
+		socketPath := path.Join(appDir, "app.sock")
+		path.Join(appDir, "app.sock")
+		logger.Debugf("socket path: %s", socketPath)
+
+		logger.Info("create unix socket")
+		listener, listenErr = net.Listen("unix", socketPath)
+		logger.Infof("server is listening unix socket %s", socketPath)
+	} else {
+		logger.Info("listen tcp")
+		listener, listenErr = net.Listen("tcp", fmt.Sprintf("%s:%s", cfg.Listen.BindIp, cfg.Listen.Port))
+		logger.Infof("server is listening port %s:%s", cfg.Listen.BindIp, cfg.Listen.Port)
 	}
+
+	if listenErr != nil {
+		log.Fatal(listenErr)
+	}
+	
 	server := &http.Server{
 		Handler: router,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout: 15 * time.Second,
 	}
 
-	logger.Info("server is listening port 0.0.0.0:1234")
+	logger.Infof("server is listening port %s:%s", cfg.Listen.BindIp, cfg.Listen.Port)
 
 	logger.Fatal(server.Serve(listener))
 }
